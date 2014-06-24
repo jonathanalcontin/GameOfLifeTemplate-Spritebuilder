1d371abd543f61a1f08d9b1cdeffff023da38c4d
Jonathan Alcontin <jonathanalcontin@Jonathans-MacBook-Pro.local>
2014-06-24 12:56:09 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/MainScene.h
@@ -0,0 +1,13 @@
+//
+//  MainScene.h
+//  GameOfLife
+//
+//  Created by Jonathan Alcontin on 6/24/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface MainScene : CCSprite
+
+@end
